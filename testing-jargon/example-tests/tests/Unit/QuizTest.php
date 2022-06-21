3bc40a7c377339c43fe3679de7d2019f66746a64
Danyiel Colin <danyiel5978@gmail.com>
2022-06-21 13:45:53 -0500

    new file:   .editorconfig new file:   .env.example new file:   .gitattributes new file:   .gitignore new file:   .styleci.yml new file:   README.md new file:   app/Console/Kernel.php new file:   app/Exceptions/Handler.php new file:   app/Http/Controllers/Controller.php new file:   app/Http/Kernel.php new file:   app/Http/Middleware/Authenticate.php new file:   app/Http/Middleware/EncryptCookies.php new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php new file:   app/Http/Middleware/RedirectIfAuthenticated.php new file:   app/Http/Middleware/TrimStrings.php new file:   app/Http/Middleware/TrustHosts.php new file:   app/Http/Middleware/TrustProxies.php new file:   app/Http/Middleware/VerifyCsrfToken.php new file:   app/Models/User.php new file:   app/Providers/AppServiceProvider.php new file:   app/Providers/AuthServiceProvider.php new file:   app/Providers/BroadcastServiceProvider.php new file:   app/Providers/EventServiceProvider.php new file:   app/Providers/RouteServiceProvider.php new file:   artisan new file:   bootstrap/app.php new file:   bootstrap/cache/.gitignore new file:   composer.json new file:   composer.lock new file:   config/app.php new file:   config/auth.php new file:   config/broadcasting.php new file:   config/cache.php new file:   config/cors.php new file:   config/database.php new file:   config/filesystems.php new file:   config/hashing.php new file:   config/logging.php new file:   config/mail.php new file:   config/queue.php new file:   config/sanctum.php new file:   config/services.php new file:   config/session.php new file:   config/view.php new file:   database/.gitignore new file:   database/factories/UserFactory.php new file:   database/migrations/2014_10_12_000000_create_users_table.php new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php new file:   database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php new file:   database/seeders/DatabaseSeeder.php new file:   lang/en/auth.php new file:   lang/en/pagination.php new file:   lang/en/passwords.php new file:   lang/en/validation.php new file:   package.json new file:   phpunit.xml new file:   public/.htaccess new file:   public/favicon.ico new file:   public/index.php new file:   public/robots.txt new file:   resources/css/app.css new file:   resources/js/app.js new file:   resources/js/bootstrap.js new file:   resources/views/welcome.blade.php new file:   routes/api.php new file:   routes/channels.php new file:   routes/console.php new file:   routes/web.php new file:   storage/app/.gitignore new file:   storage/app/public/.gitignore new file:   storage/framework/.gitignore new file:   storage/framework/cache/.gitignore new file:   storage/framework/cache/data/.gitignore new file:   storage/framework/sessions/.gitignore new file:   storage/framework/testing/.gitignore new file:   storage/framework/views/.gitignore new file:   storage/logs/.gitignore new file:   tests/CreatesApplication.php new file:   tests/Feature/ExampleTest.php new file:   tests/TestCase.php new file:   tests/Unit/ExampleTest.php new file:   tests/Unit/QuizTest.php new file:   webpack.mix.js
    Untracked files:
    ../example-1/tests/QuizTest.php

---
+++ testing-jargon/example-tests/tests/Unit/QuizTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Quiz;
+
+class QuizTest extends TestCase
+{
+	/** @test */
+	public function test_it_consists_of_questions() {
+		$quiz = new Quiz();
+		$quiz->addQuestion(new Question("2 + 2", 4));
+		$this->assertCount(1, $quiz->questions());
+	}
+
+	public function test_it_can_be_grabed() {
+		$quiz = new Quiz();
+		$quiz->addQuestion(new Question("2 * 2", 4));
+		$question = $quiz->nextQuestion();
+		$question->answer(4);
+		$this->assertEquals(100, $quiz->grade());
+	}
+
+}
